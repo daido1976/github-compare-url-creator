1f4ceddb527d70e957c5eba76ec1ed92f3faff67
Daido Shota <daido1976@gmail.com>
2021-06-03 22:57:24 +0900

    Markup App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import "./styles.css";
+import githubLogo from "./img/github.png";
 import React, { useState } from "react";
 
 export const App = () => {
-  const useTextField = () => {
+  const useTextField = (placeholder: string) => {
     const [value, setValue] = useState("");
     const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
       setValue(e.target.value);
@@ -11,41 +12,50 @@ export const App = () => {
     return {
       type: "text",
       value,
+      placeholder,
       onChange,
     };
   };
 
-  const org = useTextField();
-  const repo = useTextField();
-  const startCommit = useTextField();
-  const endCommit = useTextField();
+  const org = useTextField("org");
+  const repo = useTextField("repo");
+  const startCommit = useTextField("startCommit");
+  const endCommit = useTextField("endCommit");
 
-  const compareUrl = `https://github.com/${org.value}/${repo.value}/compare/${startCommit.value}...${endCommit.value}`;
+  const compareUrl = `https://github.com/${org.value}/${
+    repo.value
+  }/compare/${startCommit.value.substr(0, 10)}...${endCommit.value.substr(
+    0,
+    10
+  )}`;
 
   return (
-    <div>
+    <div className="container">
+      <h2>GitHub Compare URL Creator</h2>
       <label>
-        Organization/User:&nbsp;
         <input {...org} />
       </label>
       <br />
       <label>
-        Repository:&nbsp;
         <input {...repo} />
       </label>
       <br />
       <label>
-        StartCommit:&nbsp;
         <input {...startCommit} />
       </label>
       <br />
       <label>
-        EndCommit:&nbsp;
         <input {...endCommit} />
       </label>
       <br />
       <div>
-        <a href={compareUrl} target="_blank" rel="noopener noreferrer">
+        <img className="github-logo" src={githubLogo} alt="github-logo" />
+        <a
+          className="url"
+          href={compareUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           {compareUrl}
         </a>
       </div>
