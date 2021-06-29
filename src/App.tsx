c779c4ee567c82984960a5be30fffca74583f73a
daido1976 <daido1976@gmail.com>
2021-06-29 21:18:29 +0900

    Save text field value to localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,16 @@ import githubLogo from "./img/github.png";
 import React, { useState } from "react";
 
 export const App = () => {
-  const useTextField = (props: { placeholder: string }) => {
+  const useTextField = ({
+    placeholder,
+    key,
+  }: {
+    placeholder: string;
+    key: string;
+  }) => {
     const type: "text" = "text";
-    const [value, setValue] = useState("");
+    const initialValue = localStorage.getItem(key) || "";
+    const [value, setValue] = useState(initialValue);
     const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
       setValue(e.target.value);
     };
@@ -13,24 +20,32 @@ export const App = () => {
     return {
       type,
       value,
-      placeholder: props.placeholder,
+      placeholder,
       onChange,
     };
   };
+
   const truncateCharsFrom = (text: string) => {
     return text.substr(0, 10);
   };
 
-  const orgTextField = useTextField({ placeholder: "org" });
-  const repoTextField = useTextField({ placeholder: "repo" });
-  const startTextField = useTextField({ placeholder: "start" });
-  const endTextField = useTextField({ placeholder: "end" });
+  const orgTextField = useTextField({ placeholder: "org", key: "org" });
+  const repoTextField = useTextField({ placeholder: "repo", key: "repo" });
+  const startTextField = useTextField({ placeholder: "start", key: "start" });
+  const endTextField = useTextField({ placeholder: "end", key: "end" });
 
   const compareUrl = `https://github.com/${orgTextField.value}/${
     repoTextField.value
   }/compare/${truncateCharsFrom(startTextField.value)}...${truncateCharsFrom(
     endTextField.value
   )}`;
+
+  const onSaveClick = (_e: React.MouseEvent<HTMLButtonElement>) => {
+    localStorage.setItem("org", orgTextField.value);
+    localStorage.setItem("repo", repoTextField.value);
+    localStorage.setItem("start", startTextField.value);
+    localStorage.setItem("end", endTextField.value);
+  };
 
   return (
     <div className="container">
@@ -51,6 +66,9 @@ export const App = () => {
         <input {...endTextField} />
       </label>
       <br />
+      <div className="button-wrapper">
+        <button onClick={onSaveClick}>save</button>
+      </div>
       <div>
         <img className="github-logo" src={githubLogo} alt="github-logo" />
         <a
